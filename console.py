c82cd2eeb500522a65c784c323635d283fac296b
root <toqaezzatly@gmail.com>
2024-05-19 16:19:32 -0700

    commit

---
+++ console.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """Exit the program"""
+        print("")  # Print a newline before exiting
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
+
